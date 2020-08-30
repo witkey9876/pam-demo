1af06957728cf32a21d5897ed033670afe4bac58
witkey9876 <witkey9876@qq.com>
2020-08-30 21:29:06 +0800

    setting java.compiler version

---
+++ src/test/java/com/example_demo/gdt/DroolsTest.java
@@ -0,0 +1,41 @@
+package com.example_demo.gdt;
+
+import com.example_demo.model.ActionResultDO;
+import com.example_demo.model.CustomerDO;
+import org.drools.compiler.kie.builder.impl.KieFileSystemImpl;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieRuntimeFactory;
+import org.kie.api.runtime.KieSession;
+
+import java.util.Arrays;
+
+
+public class DroolsTest {
+
+    private   KieSession kSession;
+
+    @Before
+    public void before(){
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kContainer = kieServices.getKieClasspathContainer();
+         kSession = kContainer.newKieSession();
+
+    }
+
+    @Test
+    public void testLoginValidateTest(){
+        CustomerDO customerDO  = new CustomerDO();
+        customerDO.setStatus(1);
+        customerDO.setLoginAccount("redhat");
+        ActionResultDO actionResultDO = new ActionResultDO();
+        kSession.insert(Arrays.asList(customerDO,actionResultDO));
+        kSession.fireAllRules();
+        kSession.dispose();
+        Assert.assertEquals(Boolean.TRUE,actionResultDO.getBoolResult());
+    }
+}
