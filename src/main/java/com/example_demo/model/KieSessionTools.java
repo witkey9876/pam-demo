0ae09b1743e5d8308c4daf3485740ccde9dff1c6
witkey9876 <witkey9876@qq.com>
2020-08-31 00:56:33 +0800

    use kie sessins pool

--- src/main/java/com/example_demo/model/KieSessionTools.java
+++ src/main/java/com/example_demo/model/KieSessionTools.java
@@ -2,13 +2,35 @@ package com.example_demo.model;
 
 import org.drools.core.spi.KnowledgeHelper;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.KieSessionsPool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 public class KieSessionTools {
 
+
+    private static Logger Log  = LoggerFactory.getLogger("KieSessionTools");
+    private static volatile KieSessionsPool sessionsPool;
+    private static final Integer POOL_SIZE = 100;
+
+    public static KieSessionsPool getSessionsPool(KnowledgeHelper helper){
+        if(Objects.isNull(sessionsPool)) {
+            synchronized (sessionsPool) {
+                if(Objects.isNull(sessionsPool)) {
+                    sessionsPool = helper.getKieRuntime().getKieBase().newKieSessionsPool(POOL_SIZE);
+                }
+            }
+        }
+        return sessionsPool;
+    }
+
     public static final void fired(KnowledgeHelper helper,String groupName,Object ...facts){
-        KieSession kieSession = helper.getKieRuntime().getKieBase().newKieSession();
+
+        sessionsPool = getSessionsPool(helper);
+//        KieSession kieSession = helper.getKieRuntime().getKieBase().newKieSession();
+        KieSession kieSession = sessionsPool.newKieSession();
         for(Object fact:facts) {
             kieSession.insert(fact);
         }
